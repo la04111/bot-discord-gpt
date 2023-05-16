e73fdac2b9b08583c7cbdc7419f7268cdaa85df1
Tiến Nguyễn <101681409+la04111@users.noreply.github.com>
2023-05-16 13:52:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ const bodyParser = require('body-parser');
 
 const app = express()
 app.use(bodyParser.json());
-const port = 80
+const port = 443
 
 app.get('/', function(req, res) {
   res.send('ok');
