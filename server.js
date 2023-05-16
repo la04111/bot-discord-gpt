548190d09b3199e3ae822c52f109b489066a508f
TN <longan04111@gmail.com>
2023-05-16 13:34:18 +0700

    add sv

--- server.js
+++ server.js
@@ -62,64 +62,75 @@ client.on('messageCreate', async msg =>{
 //adsd
 client.login(process.env.dis);
 
-// const express = require('express')
-// const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser');
 
-// const app = express()
-// app.use(bodyParser.json());
-// const port = 3000
+const app = express()
+app.use(bodyParser.json());
+const port = 3000
 
-// app.get('/', function(req, res) {
-//   res.send('ok');
-// });
-// app.post('/', async (req, res) => {
-//    const text = req.body.cauhoi
-//     if (text) {
-//         const resgetChat = await getChat(text);
-//         if (resgetChat) {
-//           try {
+app.get('/', function(req, res) {
+  res.send('ok');
+});
+app.post('/', async (req, res) => {
+   const text = req.body.cauhoi
+    if (text) {
+        const resgetChat = await getChat(text);
+        if (resgetChat) {
+          try {
             
-//            res.send(resgetChat)
-//           } catch (error) {
-//             // ignore the error
-//           }
-//         }
-//       } else {
-//         try {
-//             res.send(
-//             "Vui lòng hỏi bất kỳ điều gì sau /hoi"
-//           );
-//         } catch (error) {
-//           // ignore the error
-//         }
-//       }
-// })
+           res.send(resgetChat)
+          } catch (error) {
+            // ignore the error
+          }
+        }
+      } else {
+        try {
+            res.send(
+            "Vui lòng hỏi bất kỳ điều gì sau /hoi"
+          );
+        } catch (error) {
+          // ignore the error
+        }
+      }
+})
 
-// app.post('/anh', async (req, res) => {
-//    const text = req.body.cauhoi
-//     if (text) {
-//         const resgetChat = await getImage(text);
-//         if (resgetChat) {
-//           try {
+app.post('/anh', async (req, res) => {
+   const text = req.body.cauhoi
+    if (text) {
+        const resgetChat = await getImage(text);
+        if (resgetChat) {
+          try {
          
-//            res.send(resgetChat)
-//           } catch (error) {
-//             // ignore the error
-//           }
-//         }
-//       } else {
-//         try {
-//             res.send(
-//             "Vui lòng hỏi bất kỳ điều gì sau /hoi"
-//           );
-//         } catch (error) {
-//           // ignore the error
-//         }
-//       }
-// })
+           res.send(resgetChat)
+          } catch (error) {
+            // ignore the error
+          }
+        }
+      } else {
+        try {
+            res.send(
+            "Vui lòng hỏi bất kỳ điều gì sau /hoi"
+          );
+        } catch (error) {
+          // ignore the error
+        }
+      }
+})
 
+const os = require('os');
 
+const interfaces = os.networkInterfaces();
 
-// app.listen(port, () => {
-//   console.log(`Server listening at http://localhost:${port}`)
-// })
+Object.keys(interfaces).forEach((ifaceName) => {
+  const iface = interfaces[ifaceName];
+  iface.forEach((alias) => {
+    if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+      console.log(`Your IP address is: ${alias.address}`);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
