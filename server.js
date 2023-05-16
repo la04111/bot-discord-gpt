684a8f2ca34dbfee4d7be4f59af71dc2ec9a64e7
TN <longan04111@gmail.com>
2023-05-16 08:12:48 +0700

    tt

--- server.js
+++ server.js
@@ -43,66 +43,66 @@ client.on('messageCreate', async msg =>{
 //
 client.login(process.env.dis);
 
-// const express = require('express')
-// const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser');
 
-// const app = express()
-// app.use(bodyParser.json());
-// const port = 3000
+const app = express()
+app.use(bodyParser.json());
+const port = 3000
 
-// const configuration = new Configuration({
-//     apiKey: process.env.API,
-//   });
-//   const openai = new OpenAIApi(configuration);
-//   module.exports = openai;
-// app.post('/', async (req, res) => {
-//    const text = req.body.cauhoi
-//     if (text) {
-//         const resgetChat = await getChat(text);
-//         if (resgetChat) {
-//           try {
-//             console.log(resgetChat)
-//            res.send(resgetChat)
-//           } catch (error) {
-//             // ignore the error
-//           }
-//         }
-//       } else {
-//         try {
-//             res.send(
-//             "Vui lòng hỏi bất kỳ điều gì sau /hoi"
-//           );
-//         } catch (error) {
-//           // ignore the error
-//         }
-//       }
-// })
+const configuration = new Configuration({
+    apiKey: process.env.API,
+  });
+  const openai = new OpenAIApi(configuration);
+  module.exports = openai;
+app.post('/', async (req, res) => {
+   const text = req.body.cauhoi
+    if (text) {
+        const resgetChat = await getChat(text);
+        if (resgetChat) {
+          try {
+            console.log(resgetChat)
+           res.send(resgetChat)
+          } catch (error) {
+            // ignore the error
+          }
+        }
+      } else {
+        try {
+            res.send(
+            "Vui lòng hỏi bất kỳ điều gì sau /hoi"
+          );
+        } catch (error) {
+          // ignore the error
+        }
+      }
+})
 
-// app.post('/anh', async (req, res) => {
-//    const text = req.body.cauhoi
-//     if (text) {
-//         const resgetChat = await getImage(text);
-//         if (resgetChat) {
-//           try {
-//             console.log(resgetChat)
-//            res.send(resgetChat)
-//           } catch (error) {
-//             // ignore the error
-//           }
-//         }
-//       } else {
-//         try {
-//             res.send(
-//             "Vui lòng hỏi bất kỳ điều gì sau /hoi"
-//           );
-//         } catch (error) {
-//           // ignore the error
-//         }
-//       }
-// })
+app.post('/anh', async (req, res) => {
+   const text = req.body.cauhoi
+    if (text) {
+        const resgetChat = await getImage(text);
+        if (resgetChat) {
+          try {
+            console.log(resgetChat)
+           res.send(resgetChat)
+          } catch (error) {
+            // ignore the error
+          }
+        }
+      } else {
+        try {
+            res.send(
+            "Vui lòng hỏi bất kỳ điều gì sau /hoi"
+          );
+        } catch (error) {
+          // ignore the error
+        }
+      }
+})
 
 
 
-// app.listen(port, () => {
-//   console.log(`Server listening at http://localhost:${port}`)
-// })
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
